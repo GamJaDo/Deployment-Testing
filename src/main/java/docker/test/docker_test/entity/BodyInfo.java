2cea33c17c9d66e93f77ac93be8c899d6a64d52b
unknown <kim0615157@gmail.com>
2024-11-13 02:15:37 +0900

    chore

--- src/main/java/docker/test/docker_test/entity/BodyInfo.java
+++ src/main/java/docker/test/docker_test/entity/BodyInfo.java
@@ -17,7 +17,7 @@ public class BodyInfo {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Column(name = "inbody_image_url")
